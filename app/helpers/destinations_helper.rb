3f325c88c8cf5b45a1e6d1c7256d3979366dc829
Aldrin Dela Cruz <adelacruz0930@gmail.com>
2021-04-22 15:25:23 +0800

    Fix number fields in forms

--- app/helpers/destinations_helper.rb
+++ app/helpers/destinations_helper.rb
@@ -1,2 +1,9 @@
 module DestinationsHelper
+  def references(destination)
+    destination.references.map(&:ref_code)
+  end
+
+  def categories(destination)
+    destination.categories.map(&:name)
+  end
 end
