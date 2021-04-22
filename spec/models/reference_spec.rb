1d60adcd50eecf604d897ab30e9b850370c1270a
Aldrin Dela Cruz <adelacruz0930@gmail.com>
2021-04-22 15:41:40 +0800

    Fix spec for product destination relationship

--- spec/models/reference_spec.rb
+++ spec/models/reference_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Reference, type: :model do
   describe '#relationships' do
-    it { should belong_to(:destination) }
+    it { should belong_to(:destination).optional(true) }
   end
 
   describe '#validations' do
